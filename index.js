433a1e8d58f89700c020e3f3b484c3cdb8014415
Lori Henderson <lorim95@gmail.com>
2022-05-20 13:37:44 -0400

    commit tree 4ea1d4f289cfb3a7d974fa201ca8c17a5fe9eef5

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Lori",
+    streetAddress: "123 Main",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {
+        ...employee,
+        [key]: value,
+    }
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {
+        ...employee,
+    }
+    console.log(newEmployee);
+    delete newEmployee[key];
+    console.log(newEmployee);
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
